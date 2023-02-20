9c53aaa12968bb185b8dd072f6bf3596831a057d
Sami Capaldi <sami.wood@tribusdigital.com>
2023-02-20 15:51:25 +0000

    Sizing amends

--- src/components/FullWidthBanner.js
+++ src/components/FullWidthBanner.js
@@ -4,8 +4,8 @@ import blackout from '/public/images/thameen-london-blackout.webp'
 export default function FullWidthBanner() {
   return (
     <main className="banner-container">
-      <div className="relative w-full h-full">
-        <Image src={blackout} width={1920} height={1080} style={{objectFit: 'cover', objectPosition: 'center', width: '100vw', height: '100vh',}} priority alt="Thameen London Reboot" id="fw-banner" />
+      <div className="">
+        <Image src={blackout} width={1920} height={1080} style={{objectFit: 'cover', objectPosition: 'center'}} priority alt="Thameen London Reboot" id="fw-banner" />
       </div>
     </main>
   )
